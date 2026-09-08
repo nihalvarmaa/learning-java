848091b583114648e0939c6cf16562725ad52241
Nihal <nihalgottumukkala@gmail.com>
2026-09-08 13:48:12 +0530

    Add scanner input program

--- Basics/ScannerInput.java
+++ Basics/ScannerInput.java
@@ -1,25 +1,27 @@
-
 import java.util.Scanner;
 
-public class Main {
-
+public class ScannerInput {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.print("Enter an integer: ");
         int a = scanner.nextInt();
+
+        System.out.print("Enter a float: ");
         float b = scanner.nextFloat();
+
+        System.out.print("Enter a double: ");
         double c = scanner.nextDouble();
+
+        System.out.print("Enter a character: ");
         char d = scanner.next().charAt(0);
-        scanner.nextLine();
-        var e = scanner.next();
-        scanner.nextLine();
-        String f = scanner.nextLine();
-        
+
         System.out.println("Integer: " + a);
         System.out.println("Float: " + b);
         System.out.println("Double: " + c);
         System.out.println("Character: " + d);
-        System.out.println("Single-word String: " + e);
-        System.out.println("String line: " + f);
+
+        scanner.close();
     }
 }
